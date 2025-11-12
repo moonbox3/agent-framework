483bd880a970ddfa4672f79a7f67e1106720c960
Evan Mattson <evan.mattson@microsoft.com>
2025-11-12 14:41:08 +0900

    Revert port test change

--- python/packages/ag-ui/agent_framework_ag_ui_examples/server/main.py
+++ python/packages/ag-ui/agent_framework_ag_ui_examples/server/main.py
@@ -112,7 +112,7 @@ add_agent_framework_fastapi_endpoint(
 
 def main():
     """Run the server."""
-    port = int(os.getenv("PORT", "8887"))
+    port = int(os.getenv("PORT", "8888"))
     host = os.getenv("HOST", "127.0.0.1")
 
     print(f"\nAG-UI Examples Server starting on http://{host}:{port}")

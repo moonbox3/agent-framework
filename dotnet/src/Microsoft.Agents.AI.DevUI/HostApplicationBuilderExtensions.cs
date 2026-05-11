117da53f2a532aeff85aa72d41cda421165d3036
Evan Mattson <evan.mattson@microsoft.com>
2026-05-11 18:13:57 +0900

    .NET: DevUI: add configurable access controls for the DevUI HTTP surface

--- dotnet/src/Microsoft.Agents.AI.DevUI/HostApplicationBuilderExtensions.cs
+++ dotnet/src/Microsoft.Agents.AI.DevUI/HostApplicationBuilderExtensions.cs
@@ -1,4 +1,6 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.Agents.AI.DevUI;
 
 namespace Microsoft.Extensions.Hosting;
 
@@ -11,12 +13,13 @@ public static class MicrosoftAgentAIDevUIHostApplicationBuilderExtensions
     /// Adds DevUI services to the host application builder.
     /// </summary>
     /// <param name="builder">The <see cref="IHostApplicationBuilder"/> to configure.</param>
+    /// <param name="configure">Optional callback used to configure <see cref="DevUIOptions"/>.</param>
     /// <returns>The <see cref="IHostApplicationBuilder"/> for method chaining.</returns>
-    public static IHostApplicationBuilder AddDevUI(this IHostApplicationBuilder builder)
+    public static IHostApplicationBuilder AddDevUI(this IHostApplicationBuilder builder, Action<DevUIOptions>? configure = null)
     {
         ArgumentNullException.ThrowIfNull(builder);
 
-        builder.Services.AddDevUI();
+        builder.Services.AddDevUI(configure);
 
         return builder;
     }

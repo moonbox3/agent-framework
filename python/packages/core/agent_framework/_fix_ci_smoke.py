480c9dfe82352bb2dacda0758b2f7dae5630de85
Evan Mattson <evan.mattson@microsoft.com>
2026-09-23 09:34:16 +0900

    Python: Use a typing failure for fix-ci smoke testing

---
+++ python/packages/core/agent_framework/_fix_ci_smoke.py
@@ -0,0 +1,6 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+
+def fix_ci_smoke(value: object) -> str:
+    """Return a value for the disposable fix-ci typing test."""
+    return value
--- python/packages/core/tests/test_fix_ci_smoke.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Microsoft. All rights reserved.
-
-
-def test_fix_ci_smoke() -> None:
-    values=[1,2,3]
-    assert sum(values)==6
